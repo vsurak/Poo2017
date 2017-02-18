cad143e035128183fb0a6a3eb4f6a9cb6daf7292
ENTERPRISE1701E\rodri <vsurak@gmail.com>
2017-02-18 09:58:50 -0600

    Actualizacion con clases

---
+++ ModelajeIntro/src/modelos/basic/Radio.java
@@ -0,0 +1,50 @@
+package modelos.basic;
+
+public class Radio 
+{
+	private short Volumen;
+	private short Frecuencia;
+	
+	
+	public short getVolumen() {
+		return Volumen;
+	}
+	
+	public void setVolumen(short volumen) {
+		Volumen = volumen;
+	}
+	
+	public short getFrecuencia() {
+		return Frecuencia;
+	}
+	
+	public void setFrecuencia(short frecuencia) {
+		Frecuencia = frecuencia;
+	}
+	
+	public void incrementarVolumen(short pCantidad)
+	{
+		
+	}
+	
+	public void decrementarVolumen(short pCantidad)
+	{
+		
+	}
+	
+	public void incrementarFrecuencia()
+	{
+		
+	}
+	
+	public void decrementarFrecuencia()
+	{
+		
+	}
+	
+	public void seek()
+	{
+		
+	}
+	
+}
